4e9137f6969bde195fb3a5bcef5dbb14b80b29cb
Ipseeka Malla <ipseeka.malla1@gmail.com>
2025-12-07 15:05:12 -0500

    project setup install

---
+++ prisma/seed.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const hashedPassword = await bcrypt.hash("Admin@123", 10);
+
+  await prisma.admin.upsert({
+    where: { email: "admin@medical.com" },
+    update: {},
+    create: {
+      email: "admin@medical.com",
+      password: hashedPassword
+    },
+  });
+
+  console.log("Admin seeded successfully!");
+}
+
+main()
+  .then(() => prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
